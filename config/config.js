77a2ffd4daa797529ccae3015ead28179e705649
20221174 <20221174@sungshin.ac.kr>
2025-05-27 10:35:55 +0900

    fix: charset 추가

--- config/config.js
+++ config/config.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
- module.exports= {
- development: { 
-username: process.env.DB_USER,
- password: process.env.DB_PW,
- database: process.env.DB_NAME,
- host: process.env.DB_HOST,
- dialect: "mysql"
- },
- production: {
- username: process.env.DB_USER,
- password: process.env.DB_PW,
- database: process.env.DB_NAME,
- host: process.env.DB_HOST,
- dialect: "mysql",
- logging: false
- }
- }
-
- 
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8mb4",
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      charset: "utf8mb4",
+    },
+  },
+};
