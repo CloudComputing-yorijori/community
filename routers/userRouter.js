f80be2064c731245d27c193210e5156dfd016213
dhstndhr <88646699+dhstndhr@users.noreply.github.com>
2025-06-02 18:29:27 +0900

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userApiController = require("../controllers/userApiController");
-const userController = require("../controllers/userController");
 //userApi
 router.get("/user/:userId/Info",userApiController.getUserInfo);
-//user
-router.get("/user/:userId/posts",userController.getMyPosts);
-router.get("/user/:userId/comments",userController.getMyComments);
-router.get("/user/:userId/saves", userController.getSavedPosts);
+router.get("/user/:userId/posts",userApiController.getMyPosts);
+router.get("/user/:userId/comments",userApiController.getMyComments);
+router.get("/user/:userId/saves", userApiController.getSavedPosts);
 
 module.exports = router;
