e43e6803670847ec18fb410a1c3041b55d7380ee
seoyoon <20221174@sungshin.ac.kr>
2025-06-02 19:48:07 +0900

    fix: url 대문자 제거

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userApiController = require("../controllers/userApiController");
 //userApi
-router.get("/:userId/Info",userApiController.getUserInfo);
+router.get("/:userId/info",userApiController.getUserInfo);
 router.get("/:userId/posts",userApiController.getMyPosts);
 router.get("/:userId/comments",userApiController.getMyComments);
 router.get("/:userId/saves", userApiController.getSavedPosts);
