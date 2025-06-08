783fbb2dff90e71c74873365abb7c375b9f5c1b7
20221174 <20221174@sungshin.ac.kr>
2025-06-08 15:55:33 +0900

    fix: 이미지 업로드로 된 경로 수정

--- routers/writeRouter.js
+++ routers/writeRouter.js
@@ -20,7 +20,7 @@ const upload = multer({
 router.get("/", writeController.getWritePage); //글쓰기페이지 이동
 router.get("/getIngredients", writeController.getIngredients); //재료 검색(요청)시 컨트롤러로 넘김
 router.get("/getMenu", writeController.getMenu); // 메뉴 검색(요청) 시 컨트롤러로 넘김
-router.post("/postWrite", upload.single("img"), writeController.postImage); // 글을 처리해줄 경로
+router.post("/postWrite", writeController.postWrite); // 글을 처리해줄 경로
 router.get("/getWritedPage", writeController.getWritedPage);
 router.post("/postCommentPage", writeController.postCommentPage);
 router.post("/commentUpdate", writeController.commentUpdate);
