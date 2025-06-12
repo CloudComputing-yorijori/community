f3c821c46f9cb1992d4ce0090d1349cbea9e83be
20221174 <20221174@sungshin.ac.kr>
2025-06-12 23:52:48 +0900

    feat: home 컨트롤러 및 라우터 추가

---
+++ routers/homeRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/homeController');
+
+
+router.get('/home', homeController.getHomePage);
+router.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/posts/loginRecommend');
+    } else {
+        res.redirect('/posts/noLoginRecommend');
+    }
+});
+
+module.exports = router;
